a57da663bd410eed0fae5daa1a9ac58195c93e2a
ivan-adi-saputra <ivanadisaputra34@gmail.com>
2024-09-12 19:58:32 +0700

    slicing detail company page

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,14 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+import dayjs from "dayjs";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const dateFormat = (
+  date: Date | string,
+  format: string = "DD MMM YYYY"
+) => {
+  return dayjs(date).format(format);
+};
